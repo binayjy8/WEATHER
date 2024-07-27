828b72d3419a47a373d64a801d15edf033b0069f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-27 20:20:09 +0530

    react code

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,7 +1,17 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css"
+
 export default function SearchBox() {
     return (
-        <div>
+        <div className="SearchBox">
             <h3>Search for the Weather</h3>
+            <form>
+            <TextField id="city" label="City Name" variant="outlined" required/>
+            <br></br>
+            <br></br>
+            <Button variant="contained" type="submit">Search</Button>
+            </form>
         </div>
     )
 }
